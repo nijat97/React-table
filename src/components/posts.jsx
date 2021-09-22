7b4f9936c4ec1cd039f6d997fbc52e497354a089
nijat97 <nijathasanov97@gmail.com>
2021-09-22 23:39:18 +0200

    New readings are shown on the table now
    New reading with the same ID does not update
    the element but gets added as new element.

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -2,16 +2,61 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 const Posts = () => {
-    const [posts, setPosts] = useState();
+    // const [posts, setPosts] = useState(deviceList);
+    const [theArray, setTheArray] = useState([]);
+
+    
+
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then(res=>{
-                console.log(res.data);
-                setPosts(res.data);
-        })
+        // axios.get('https://jsonplaceholder.typicode.com/todos')
+        // .then(res=>{
+        //         console.log(res.data);
+        //         setPosts(res.data);
+        // });
+        const updateItem =(id, newvalue)=> {
+            
+            const updatedValue = theArray.map((arr)=>
+                arr.id === id ? newvalue : arr  
+            );
+            if(updatedValue === newvalue)
+            {
+                console.log('same')
+            }
+            else
+            {
+                console.log('notfound')
+                setTheArray(arr => [...arr, newvalue]);
+            }
+            // if (index === -1){
+            //   // handle error
+            //   setTheArray(arr => [...arr, newvalue]);
+            //   console.log(Object.keys(theArray).length)
+            //   console.log('index=-1')
+              
+            // }
+            // else
+            // {
+            //   setTheArray([
+            //     ...theArray.slice(0,index),
+            //     newvalue,
+            //     ...theArray.slice(index+1)
+            //   ]
+            //           );
+            // console.log('index!=-1');
+            // }
+          }
+    
+        window.addEventListener("new_readings", (event) => {
+            console.log("new_readings",event.detail);
+            //setTheArray(arr => [...arr, event.detail]);
+            console.log(event.detail.id);
+            updateItem(event.detail.id, event.detail)
+            console.log(theArray.at(1));
+            });
     },[])
+
     return ( <div>{
-        !posts ? ("No data found "):(
+        !theArray ? ("No data found "):(
             <table className='table'>
                 <thead>
                     <tr>
@@ -23,23 +68,39 @@ const Posts = () => {
                 </thead>
                 <tbody>
                     {
-                    posts.map((post, index)=>(
-                        <tr key={index}>
-                            <td>{post.id}</td>
-                            <td>{post.userId}</td>
-                            <td>{post.title}</td>
-                            <td>
-                                <p
-                                    className={
-                                        post.completed ? "btn btn-success" : "btn btn-danger"
-                                    }>
-                                    {
-                                        post.completed ? "ON" : "OFF"    
-                                    }
-                                </p>
-                            </td>
-                        </tr>
-                    ))}
+                        theArray.map(device => (
+                            <tr key={device.id}>
+                                <td>{device.id}</td>
+                                <td>{device.temperature}</td>
+                                <td>{device.humidity}</td>
+                                <td>{device.readingId}</td>
+                            </tr>
+                        
+                        // <tr key={index}>
+                        //     <td>{post.id}</td>
+                        //     <td>{post.userId}</td>
+                        //     <td>{post.title}</td>
+                        //     <td>
+                        //         <p
+                        //             className={
+                        //                 post.completed ? "btn btn-success" : "btn btn-danger"
+                        //             }>
+                        //             {
+                        //                 post.completed ? "ON" : "OFF"    
+                        //             }
+                        //         </p>
+                        //         <p
+                        //             className={
+                        //                 post.completed ? "btn btn-success" : "btn btn-danger"
+                        //             }>
+                        //             {
+                        //                 post.completed ? "ON" : "OFF"    
+                        //             }
+                        //         </p>
+                        //     </td>
+                        // </tr>
+                   ))
+                    }
                 </tbody>
             </table>
         )
