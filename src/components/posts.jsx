d53546dcf7308ce69b964356bb1163e8c7df0e23
nijat97 <nijathasanov97@gmail.com>
2021-11-05 10:38:51 +0100

    GUI lag on setState is eliminated
    Signed-off-by: nijat97 <nijathasanov97@gmail.com>

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -3,19 +3,20 @@ import axios from "axios";
 
 const Posts = () => {
     const [post, setPost] = useState([]);
-    const [pairs, setPairs] = useState([]);
+    const [pairs] = useState([]);
+    const [,setGui] = useState(0);
 
     const handleChange = (n, val) => {
-        const name = parseInt(n,10);
-        const value = parseInt( (val === true || val === false) ? + val : val);
+        const name = parseInt(n, 10);
+        const value = parseInt((val === true || val === false) ? + val : val);
         const i = pairs.findIndex(element => element[0] === name)
-        if(i === -1)
-        {
+        if (i === -1) {
             pairs.push([name, value]);
+            setGui(current => !current);
         }
-        else
-        {
-            pairs[i]=[name, value];
+        else {
+            pairs[i] = [name, value];
+            setGui(current => !current);
         }
 
         console.log("handle", pairs)
@@ -24,8 +25,7 @@ const Posts = () => {
     const sendPostRequest = async (dev) => {
 
         const element = pairs.find(e => e[0] === dev.ID);
-        if(element[1] === NaN)
-        {
+        if (isNaN(element[1])) {
             alert("Set value before sending!");
             return;
         }
@@ -52,7 +52,7 @@ const Posts = () => {
         //console.log("Pairs:",pairs)
         pairs.forEach(item => {
             if (post.find((element) => element.ID === item[0]) === undefined) {
-                pairs.splice(pairs.findIndex(item),1)
+                pairs.splice(pairs.findIndex(item), 1)
             }
         })
     }, [post])
@@ -84,10 +84,8 @@ const Posts = () => {
         return () => source.removeEventListener('new_readings', callBackForEvent)
     }, [callBackForEvent])
 
-    const renderKey = (key) =>
-    {
-        switch(key)
-        {
+    const renderKey = (key) => {
+        switch (key) {
             case 1:
                 return "Temperature";
             case 2:
@@ -102,42 +100,36 @@ const Posts = () => {
         }
     }
 
-    const renderInput = (device) =>
-    {
-        if(device.key === 1)
-        return(
-            <input
-            type="number"
-            name={device.ID}
-            value={pairs.find(element => element[0] === device.ID)[1] || ''}
-            onChange={e => handleChange(e.target.name, e.target.value)}
-            />
-        )
-        else if(device.key === 2)
-        {
-            return(<div>Unavailable</div>)
+    const renderInput = (device) => {
+        if (device.key === 1)
+            return (
+                <input
+                    type="number"
+                    name={device.ID}
+                    value={pairs.find(element => element[0] === device.ID)[1] || ''}
+                    onChange={e => handleChange(e.target.name, e.target.value)}
+                />
+            )
+        else if (device.key === 2) {
+            return (<div>Unavailable</div>)
         }
-        else if(device.key === 3 || device.key === 4)
-        {
-            return(
-                <input 
+        else if (device.key === 3 || device.key === 4) {
+            return (
+                <input
                     type="checkbox"
                     name={device.ID}
                     checked={pairs.find(element => element[0] === device.ID)[1] || ''}
-                    onChange={e => handleChange(e.target.name,e.target.checked)}
+                    onChange={e => handleChange(e.target.name, e.target.checked)}
                 />
             )
         }
-        else
-        {
-            return(<div>Unknown key</div>)
+        else {
+            return (<div>Unknown key</div>)
         }
     }
 
-    const renderValue = (device) =>
-    {
-        switch(device.key)
-        {
+    const renderValue = (device) => {
+        switch (device.key) {
             case 1:
                 return String(device.value) + "'C";
             case 2:
@@ -152,7 +144,7 @@ const Posts = () => {
         }
     }
 
-    return (<> <div>{
+    return (<div>{
         !post ? ("No data found ") : (
             <table className='table'>
                 <thead>
@@ -169,38 +161,31 @@ const Posts = () => {
                 <tbody>
                     {
                         post.map(device => (
-                            <React.Fragment key={device.ID}>
-                                <tr>
-                                    <td rowSpan={device.length + 1}>
-                                        {device.ID}</td>
-                                    <td rowSpan={device.length + 1}>
-                                        {device.sender}</td>
-                                    <td rowSpan={device.length + 1}>
-                                        {device.readingId}</td>
-                                    <td rowSpan={device.length + 1}>
-                                        {renderKey(device.key)}</td>
-                                    <td rowSpan={device.length + 1}>
-                                        {renderValue(device)}</td>
-                                    <td rowSpan={device.length + 1}>
-                                        <div>
-                                            <label>
-                                                {renderInput(device)}
-                                            </label>
-                                        </div></td>
-                                    <td rowSpan={device.length + 1}>
-                                        <div><button type="button" class="btn btn-success" onClick={() => sendPostRequest(device)} disabled={device.key === 2 ? 1 : 0 }>Send</button></div></td>
-                                </tr>
-                            </React.Fragment>
+                            <tr key={device.ID}>
+                                <td rowSpan={device.length + 1}>
+                                    {device.ID}</td>
+                                <td rowSpan={device.length + 1}>
+                                    {device.sender}</td>
+                                <td rowSpan={device.length + 1}>
+                                    {device.readingId}</td>
+                                <td rowSpan={device.length + 1}>
+                                    {renderKey(device.key)}</td>
+                                <td rowSpan={device.length + 1}>
+                                    {renderValue(device)}</td>
+                                <td rowSpan={device.length + 1}>
+                                    <div>
+                                        <label>
+                                            {renderInput(device)}
+                                        </label>
+                                    </div></td>
+                                <td rowSpan={device.length + 1}>
+                                    <div><button type="button" class="btn btn-success" onClick={() => sendPostRequest(device)} disabled={device.key === 2 ? 1 : 0}>Send</button></div></td>
+                            </tr>
                         ))}
                 </tbody>
             </table>
-
-        )
-
-    }
+        )}
     </div>
-
-    </>
     );
 }
 
